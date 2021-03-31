37d6129e71f5695686ab62782e8b548f732d38ab
Greem666 <29668023+Greem666@users.noreply.github.com>
2021-03-31 21:31:30 +1100

    Exercise 4.3 - WIP

---
+++ ex4_3/src/main/java/com/kodillapro/ex4_3/Ex43Application.java
@@ -0,0 +1,13 @@
+package com.kodillapro.ex4_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex43Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex43Application.class, args);
+    }
+
+}
---
+++ ex4_3/src/test/java/com/kodillapro/ex4_3/Ex43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodillapro.ex4_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex43ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
